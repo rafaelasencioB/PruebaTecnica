b354a70dfb0037578f0173226dee019f52b06351
rafaelasencioB <rafaelasenciodeveloper@gmail.com>
2021-12-02 13:03:32 +0100

    Create Util with method to decode JSON

---
+++ TechnicalTest/Utils.swift
@@ -0,0 +1,24 @@
+//
+//  Utils.swift
+//  TechnicalTest
+//
+//  Created by Rafael Asencio on 2/12/21.
+//
+
+import Foundation
+
+class Utils {
+    
+    static let jsonDecoder: JSONDecoder = {
+        let jsonDecoder = JSONDecoder()
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        jsonDecoder.dateDecodingStrategy = .formatted(dateFormatter)
+        return jsonDecoder
+    }()
+    
+    static let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-mm-dd"
+        return dateFormatter
+    }()
+}
